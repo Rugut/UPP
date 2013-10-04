a96a7d72f4fae76c545fcb46fc7a4c8dd2296c88
Rugut <Rugut@yandex.ru>
2013-10-04 14:21:37 +0400

    20130410

--- Ext/desktop/Справочники/ВидыПараметровВыпускаПродукции/ФормаВыбораГруппыУправляемая.js
+++ Ext/desktop/Справочники/ВидыПараметровВыпускаПродукции/ФормаВыбораГруппыУправляемая.js
@@ -3,6 +3,7 @@
 	Ext.define('Справочники.ВидыПараметровВыпускаПродукции.ФормаВыбораГруппыУправляемая',
 	{
 	extend: 'Ext.window.Window',
+	id: 'ФормаВыбораГруппыУправляемая',
 	style: 'position:absolute;width:0px;height:0px;',
 	iconCls: 'bogus',
 	minimizable: true,
@@ -14,6 +15,7 @@
 	items:
 	[{
 		xtype: 'form',
+		ПервоеОткрытие: true,
 		items:
 		[
 		],
@@ -22,4 +24,30 @@
 	[
 	]
 	});
+	listeners:
+	{
+		resize:
+		{
+			fn: function (win, width, height, opt)
+			{
+				var форма = win.down('form');
+				if (!форма.ПервоеОткрытие)
+				{
+					форма.items.each(function (item)
+					{
+					ПривязкаГраниц(item, item.ПозицияЭлемента);
+					if (item.Групповой)
+					{
+						var элемент = Ext.getCmp(item.id).items.items[0].items.items;
+						for (i = 0; i <= элемент.length -1; i += 1) 
+						{
+							var текЭлемент = элемент[i];
+							ПривязкаГраниц(текЭлемент, текЭлемент.ПозицияЭлемента);
+						}
+					}
+					});
+				}
+			}
+		}
+	}
 });
